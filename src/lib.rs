3475ecf9aa6552338d8b7c9c6a0ea1c117088791
James Cauwelier <james@accelerated.dev>
2024-01-05 13:56:05 -0800

    Reconfigure testing and naming of action steps

--- src/lib.rs
+++ src/lib.rs
@@ -1,6 +1,13 @@
 #![deny(warnings)]
 #![allow(dead_code, unused_imports)]
 
+/// A sample documentation test
+///
+/// # Example
+///
+/// ```
+/// assert!(true);
+/// ```
 fn hello() -> String {
     "Hello".into()
 }
